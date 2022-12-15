dcf3dee78b5d5255b0dda9603f00e2407852f079
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-12-15 16:47:48 -0600

    [Update] Framer motion

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
+import { motion } from "framer-motion"
+import { v4 as uuidv4 } from 'uuid';
 import logo from "../public/logo.svg"
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
 import Image from 'next/image'
+import { Squash as Hamburger } from 'hamburger-react'
 
 type Props = {}
 
@@ -9,23 +12,92 @@ const Header = (props: Props) => {
 
     const [menu, setMenu] = useState(false)
 
-    const menuItems: String[] = ["Home", "About", "Work", "Skills", "Contact"]
+    const menuItems: { name: String, duration: Number }[] = [
+        {
+            name: "Home",
+            duration: 0.6
+        },
+        {
+            name: "About",
+            duration: 0.7
+        },
+        {
+            name: "Work",
+            duration: 0.8
+        },
+        {
+            name: "Skills",
+            duration: 0.9
+        },
+        {
+            name: "Contact",
+            duration: 1
+        }
+    ]
 
     return (
-        <div>
-            <div className={`flex justify-center items-center w-full h-12 text-secondary
-        px-4 fixed shadow-xl z-50 transition-all ease-in duration-300 ${menu ? 'bg-black/90' : 'backdrop-blur bg-black/70'}`}>
-                <div className="max-w-screen-lg flex justify-between items-center w-full">
+        <>
+            <header className="fixed flex justify-center items-center w-full h-12 text-secondary
+        px-4 shadow-xl transition-all ease-in duration-300 bg-black/90 backdrop-blur">
+                <nav className="max-w-screen-lg flex justify-between items-center w-full">
 
-                    <Image priority={true} className="z-30 w-14 cursor-pointer hover:scale-95 duration-500" src={logo} alt="Logo" />
+                    <motion.div
+                        initial={{
+                            x: 500,
+                            y: 500,
+                            opacity: 0,
+                            scale: 2
+                        }}
+                        animate={{
+                            x: 0,
+                            y: 0,
+                            opacity: 1,
+                            scale: 1
+                        }}
+                        transition={{
+                            duration: 0.7
+                        }}
+                    >
+                        <Image priority={true} className="w-14 cursor-pointer hover:scale-95 duration-500" src={logo} alt="Logo" />
+                    </motion.div>
 
-                    <div className="hidden md:flex gap-5">
-                        {menuItems.map((eachItem, index) => (
-                            <p key={index} className="cursor-pointer text-secondary duration-300 hover:text-primary hover:underline hover:underline-offset-[6px]">
-                                {eachItem}
-                            </p>
+                    <div className="cursor-pointer md:hidden">
+                        <Hamburger toggled={menu} toggle={setMenu} color="#ffffff" />
+                    </div>
+
+                    <div
+
+                        className="hidden md:flex gap-5">
+                        {menuItems.map(({ name, duration }) => (
+                            <motion.p
+                                initial={{
+                                    y: -100,
+                                    opacity: 0,
+                                }}
+                                animate={{
+                                    y: 0,
+                                    opacity: 1,
+                                }}
+                                transition={{
+                                    duration: duration
+                                }}
+                                key={uuidv4()} className="cursor-pointer text-secondary duration-300 hover:text-primary hover:underline hover:underline-offset-[6px]">
+                                {name}
+                            </motion.p>
                         ))}
-                        <div className="md:flex gap-5">
+                        <motion.div
+                            initial={{
+                                x: 100,
+                                opacity: 0,
+                            }}
+                            animate={{
+                                x: 0,
+                                opacity: 1,
+                            }}
+                            transition={{
+                                duration: 1
+                            }}
+                            className="md:flex gap-5">
                             <a href="https://github.com/AkhilsWorkshop" target="_blank" rel="noreferrer"
                                 data-aos="fade-down"
                                 data-aos-once="true"
@@ -39,11 +111,11 @@ const Header = (props: Props) => {
                                 data-aos-delay="600">
                                 <AiFillLinkedin size={25} className="duration-300 hover:text-primary hover:scale-110" />
                             </a>
-                        </div>
+                        </motion.div>
                     </div>
-                </div>
-            </div>
-        </div>
+                </nav>
+            </header>
+        </>
     )
 }
 
